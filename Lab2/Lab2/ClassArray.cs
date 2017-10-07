b4da661ce173ecd5a6c32f6f63bc13c0c5678e2d
sofKul <31569914+sofKul@users.noreply.github.com>
2017-10-07 09:00:11 +0300

    Add files via upload

---
+++ Lab2/Lab2/ClassArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+	class ClassArray<T> where T:ITransport
+	{
+		private T[] places;
+		private T defaultValue;
+
+		public ClassArray(int size, T defVal)
+		{
+			defaultValue = defVal;
+			places = new T[size];
+			for(int i = 0; i < places.Length; i++)
+			{
+				places[i] = defaultValue;
+			}
+		}
+
+		public static int operator +(ClassArray<T> p, T plane)
+		{
+			for(int i = 0; i < p.places.Length; i++)
+			{
+				if (p.checkFree(i))
+				{
+					p.places[i] = plane;
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public static T operator -(ClassArray<T> p, int index)
+		{
+			if (!p.checkFree(index))
+			{
+				T plane = p.places[index];
+				p.places[index] = p.defaultValue;
+				return plane;
+			}
+			return p.defaultValue;
+		}
+
+		public bool checkFree(int index)
+		{
+			if (index < 0 || index > places.Length) return false;
+			if (places[index] == null) return true;
+			if (places[index].Equals(defaultValue)) return true;
+
+			return false;
+		}
+
+		public T getObject(int index)
+		{
+			if (index > -1 && index < places.Length) return places[index];
+
+			return defaultValue;
+		}
+
+	}
+}
---
+++ Lab2/Lab2/Fighter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Fighter:War
+    {
+        private bool bomb;
+        private bool gun;
+
+        private Color colorFight;
+
+        public Fighter(int maxSpeed, int fuel,int maxHeight, int weight, Color color, bool bomb,bool gun,Color colorFight):base(maxSpeed, fuel, maxHeight,weight, color)
+        {
+            this.bomb = bomb;
+            this.gun = gun;
+            this.colorFight = colorFight;
+        }
+
+        protected override void drawWarPlane(Graphics g)
+        {
+			base.drawWarPlane(g);
+
+			if (bomb)
+            {
+				Pen p = new Pen(colorFight);
+				g.DrawEllipse(p, startX, startY + 10, 15, 10);
+
+				Brush b = new SolidBrush(colorFight);
+				g.FillEllipse(b, startX, startY + 10, 15, 10);
+
+            }
+            if (gun)
+            {
+                Pen p = new Pen(colorFight);
+                g.DrawRectangle(p, startX+30, startY + 35, 30, 5);
+
+                Brush b = new SolidBrush(colorFight);
+                g.FillRectangle(b, startX+30, startY + 35, 30, 5);
+            }
+
+            
+        }
+    }
+}
